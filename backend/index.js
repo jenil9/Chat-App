ca15e09bc0a2860ae66e9e60a30c077b64cb35bf
jenil buha <jenilbuha@jenils-MacBook-Air-6.local>
2026-04-10 20:06:53 +0530

    fix: update iconv-lite for Node 22 compatibility2

--- backend/index.js
+++ backend/index.js
@@ -125,7 +125,7 @@ app.use(cookieParser());
 
 const key = fs.readFileSync('cert.key');
 const cert = fs.readFileSync('cert.crt');
-const server = null;
+let server = null;
 
 if(process.env.APP_SERVER_ENV === 'development') {server = https.createServer({ key, cert }, app)}
 else {server = https.createServer(app)}
